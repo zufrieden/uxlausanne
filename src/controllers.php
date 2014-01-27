b13049684c21345566376c375d40b40a119b18f8
Marc Friederich <marc@antistatique.net>
2014-01-27 22:37:31 +0100

    add Silex

---
+++ src/controllers.php
@@ -0,0 +1,24 @@
+<?php
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+
+$app->get('/', function () use ($app, $athletes) {
+    return $app['twig']->render('index.html.twig');
+})
+->bind('homepage')
+;
+
+$app->error(function (\Exception $e, $code) use ($app) {
+    if ($app['debug']) {
+        return;
+    }
+
+    $page = 404 == $code ? '404.html.twig' : '500.html.twig';
+
+    return new Response($app['twig']->render($page, array('code' => $code)), $code);
+});
