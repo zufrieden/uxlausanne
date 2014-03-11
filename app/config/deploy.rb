1b1aa2a5342f9e9766a8b9558f1c245e2521d9a2
Marc Friederich <marc@antistatique.net>
2014-03-11 19:10:25 -0400

    multistage extention

--- app/config/deploy.rb
+++ app/config/deploy.rb
@@ -3,6 +3,9 @@ set :application, '2014.uxlausanne.com'
 set :repository,  "git@github.com:zufrieden/uxlausanne.git"
 set :scm,         :git
 
+require 'capistrano/ext/multistage'
+set :stages, %w(dev production)
+
 # set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
