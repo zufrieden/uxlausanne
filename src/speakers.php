8053d074ac946917fb271e7092c6d6217ec62f72
Gilles D <gilles.demarty@gmail.com>
2014-03-26 18:25:29 +0100

    Hide some speakers

--- src/speakers.php
+++ src/speakers.php
@@ -2,6 +2,7 @@
 
 $app->speakers = array(
     'janina_woods' => array(
+        'display' => true,
         'slug' => 'janina_woods',
         'promoted' => false,
         'title' => 'Janina Woods',
@@ -24,7 +25,8 @@ $app->speakers = array(
     ),
     'anna_dahlstrom' => array(
         'slug' => 'anna_dahlstrom',
-        'promoted' => false,
+        'display' => true,
+        'promoted' => true,
         'title' => 'Anna Dahlström',
         'body' => 'Anna is a former Experience Lead with Dare, the Digital Communication Agency. <br/>After a couple of years, she decided to leave her position and go freelance, in order to pursue her own projects and focus more specifically on Information Architecture and User Experience, as well as getting more involved in the startup community. She’s worked for BBC News, Disney, Tropicana, Max Factor, Google, Vodafone and Johnson & Johnson. She’s also an instructor at General Assembly in London where she’s based.',
         'teaser' => 'Swedish IA+UX designer with a soft spot for cafes, challenges & flexible working.',
@@ -41,6 +43,7 @@ $app->speakers = array(
     ),
     'fabian_hemmert' => array(
         'slug' => 'fabian_hemmert',
+        'display' => true,
         'promoted' => false,
         'title' => 'Fabian Hemmert',
         'body' => 'Young researcher, Fabian has nevertheless an impressive record, as he’s already worked for Nintendo Europe et Marvel Comics. His performance talks have been noticed and much commented on during SXSW 2012 & 2013 and TEDxBerlin, especially the one devoted to shape-shifiting cell phones. Fascinated with the non-visual communication techniques, he explores the impact of feeling, weight, touch and movement. He currently works for Deutsche Telekom.',
@@ -61,6 +64,7 @@ $app->speakers = array(
         )
     ),
     'brigit_geiberger_and_peter_boersma' => array(
+        'display' => true,
         'slug' => 'brigit_geiberger_and_peter_boersma',
         'promoted' => false,
         'title' => 'Birgit Geiberger & Peter Boersma',
@@ -119,6 +123,7 @@ $app->speakers = array(
         )
     ),
     'oliver_reichenstein' => array(
+        'display' => true,
         'slug' => 'oliver_reichenstein',
         'promoted' => true,
         'title' => 'Oliver Reichenstein',
@@ -136,6 +141,7 @@ $app->speakers = array(
         )
     ),
     'nicolas_nova' => array(
+        'display' => true,
         'slug' => 'nicolas_nova',
         'promoted' => true,
         'title' => 'Nicolas Nova',
@@ -157,6 +163,7 @@ $app->speakers = array(
         )
     ),
     'valerie_vuillerat' => array(
+        'display' => true,
         'slug' => 'valerie_vuillerat',
         'promoted' => false,
         'title' => 'Valerie Vuillerat',
@@ -174,6 +181,7 @@ $app->speakers = array(
         )
     ),
     'ryan_rumsey' => array(
+        'display' => true,
         'slug' => 'ryan_rumsey',
         'promoted' => false,
         'title' => 'Ryan Rumsey',
@@ -195,6 +203,7 @@ $app->speakers = array(
         )
     ),
     'simon_raess' => array(
+        'display' => true,
         'slug' => 'simon_raess',
         'promoted' => false,
         'title' => 'Simon Raess',
@@ -212,6 +221,7 @@ $app->speakers = array(
         )
     ),
     'will_evans' => array(
+        'display' => true,
         'slug' => 'will_evans',
         'promoted' => false,
         'title' => 'Will Evans',
@@ -229,6 +239,7 @@ $app->speakers = array(
         )
     ),
     'indi_young' => array(
+        'display' => true,
         'slug' => 'indi_young',
         'promoted' => true,
         'title' => 'Indi Young',
@@ -246,8 +257,9 @@ $app->speakers = array(
         )
     ),
     'giles_colborne' => array(
+        'display' => false,
         'slug' => 'giles_colborne',
-        'promoted' => true,
+        'promoted' => false,
         'title' => 'Giles Colborne',
         'body' => 'Giles’ book \'Simple and usable web mobile and interaction design\' published by New Riders is a best-seller in the US, UK and China. He has been working in usability and user centred design since 1991 and is actively involved in the community - most recently as co-chair of IA Summit 2013.<br/><br />He founded cxpartners with Richard Caddick in 2004, which has grown to become an international consultancy which works with companies all over the world to create web and mobile user experiences that make a measurable difference to their bottom line and their customers’ lives.<br /><br />Giles is a former President of the UK Usability Professionals’ Association and has worked with British Standards Institute in developing guidance on web accessibility.',
         'teaser' => 'Managing Director of cxpartners. Author of \'Simple and Usable\'. Business Mentor. AKA 科尔伯恩 and 자일스 콜.',
