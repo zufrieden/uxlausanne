f5665ca64cb14a8ee4206f133a8f8416c5e85fd3
Gilles D <gilles.demarty@gmail.com>
2014-05-15 14:17:12 +0200

    changing style jobs

--- src/jobs.php
+++ src/jobs.php
@@ -2,22 +2,26 @@
 $app['jobs'] = $app->share(function($app) {
     // @TODO: add a cache
     $feed = 'http://freshjobs.ch/kategorie/design-jobs/feed/';
-
     $xml = simplexml_load_file($feed);
     if (false === $xml) {
         return array();
     }
-
+    $xml->registerXPathNamespace('dc', 'http://purl.org/dc/elements/1.1/');
     $jobs = array();
+
+
     foreach ($xml->xpath('//item') as $item) {
         $categories = array_map(function($cat) { return (string) $cat; }, (array) $item->category);
+            $item_dc = $item->children('http://purl.org/dc/elements/1.1/');
 
-        $job = array(
+
+            $job = array(
             'title' => (string) $item->title,
             'categories' => $categories,
             'link' => (string) $item->link, // @TODO remove or replace utm tags
             'pubDate' => (string) $item->pubDate,
             'guid' => (string) $item->guid,
+            'company' => (string) $item_dc->creator,
         );
 
         $jobs[] = $job;
